ac2d0561ff9709edfdbf8a071399c5b17ffcfab3
caramelowl <Sony.nv@yandex.ru>
2023-07-10 22:46:09 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13_676;
+        int pricePerMile = 20;
+        int bonus = price / pricePerMile;
+
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
